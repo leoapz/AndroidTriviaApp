9fbb08ba542d432be69ffe47f4818b9848fe1e26
Leonardo Apaza <leoapaza2@gmail.com>
2020-11-01 12:00:05 -0300

    Create First Navigation connection

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -13,6 +14,14 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_title, container, false)
+//        Se lo puede realizar asi tambien
+//        binding.playButton.setOnClickListener { view: View ->
+//            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+
+        binding.playButton.setOnClickListener(
+          Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
 
         return binding.root
     }
